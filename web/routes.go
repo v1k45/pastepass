f67ceb3d8e1d4582b38bc1501dfb8ff6620c7bbe
Vikas <v1k45x@gmail.com>
2024-06-02 12:45:17 +0530

    Add static file server

--- web/routes.go
+++ web/routes.go
@@ -1,6 +1,12 @@
 package web
 
-import "net/http"
+import (
+	"embed"
+	"net/http"
+)
+
+//go:embed static
+var staticFs embed.FS
 
 func (h *Handler) Router() http.Handler {
 	router := http.NewServeMux()
@@ -8,5 +14,6 @@ func (h *Handler) Router() http.Handler {
 	router.HandleFunc("POST /", h.Paste)
 	router.HandleFunc("GET /p/{id}/{key}", h.View)
 	router.HandleFunc("POST /p/{id}/{key}", h.Decrypt)
+	router.Handle("GET /static/", http.FileServer(http.FS(staticFs)))
 	return router
 }
