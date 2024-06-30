231328a6f9d0fe88b415822532c87d5c484617c9
Vikas <v1k45x@gmail.com>
2024-06-30 14:38:16 +0530

    Update database file name in config

--- config/config.go
+++ config/config.go
@@ -3,7 +3,7 @@ package config
 var (
 	ServerAddr = ":8008"
 	AppName    = "PastePass"
-	DBPath     = "pastepass.boltdb"
+	DBPath     = "pastepass.db"
 	ResetDB    = false
 	Version    = "0.00-dev"
 )
